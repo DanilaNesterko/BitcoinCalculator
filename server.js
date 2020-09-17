989b18a03b54fb49f6f94642db1ce14538d5063a
Opilane TTHK <opilane@tthk.ee>
2020-09-17 15:19:43 +0300

    Added functional

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', function(request, response){
    // console.log(request);
    // response.send('<h1>Hello, World!</h1>');
-   console.log(__dirname)
+   console.log(__dirname);
    response.sendFile(__dirname+"/index.html");
 })
 
 app.post('/', function(req,res){
-    let number1= req.body.num1;
-    let number2= req.body.num2;
-    console.log('Number 1 ', number1);
-    console.log('Number 2 ', number2);
-    res.redirect('/');
+    let number1= Number(req.body.num1);
+    let number2= Number(req.body.num2);
+
+    let result= number1 + number2;
+    console.log(req.body);
+    res.send(`${result}`);
+    // console.log('Number 1 ', number1);
+    // console.log('Number 2 ', number2);
 });
 app.listen(3000, function(){
     console.log('Server is running on Port 3000')
