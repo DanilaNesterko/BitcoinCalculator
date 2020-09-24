6cee8281fcd739bfd4ac039e9f414357811f4261
Opilane TTHK <opilane@tthk.ee>
2020-09-24 15:31:03 +0300

    Estonia

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
 const express = require('express');
-const app = express();
+const axios = require('axios');
 const bodyParser = require('body-parser');
+const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(request, response){
-   // console.log(request);
+app.get('/', function(req, res){
+   
+   //let url="https://api.coindesk.com/v1/bpi/currentprice/eur.json";
+
+   let url="https://restcountries.eu/rest/v2/name/estonia?fullText=true";
+
+   axios.get(url).
+   then((response)=> {
+    //let disclaimer = response.data.disclaimer;
+    //let eurRates = response.data.bpi.EUR.rate;
+    //let eurCode = response.data.bpi.EUR.code;
+    //let usdRates = response.data.bpi.USD.rate;
+    //let usdCode = response.data.bpi.USD.code;
+    let name = response.data[0].name;
+    let capital = response.data[0].capital;
+    let timezone = response.data[0].timezones[0];
+    let currency = response.data[0].currencies[0].name;
+    let regional_Block = response.data[0].regionalBlocs[0].name;
+
+    
+    res.write(`<p> Name - ${name} </p>`);
+    res.write(`<p> Capital - ${capital} </p>`);
+    res.write(`<p> Timezone - ${timezone} </p>`);
+    res.write(`<p> Currency - ${currency} </p>`);
+    res.write(`<p> Regional Block ${regional_Block} </p>`);
+    res.send();
+
+   }).
+   catch((error) =>{
+       console.log(error);
+   });
+    // console.log(request);
    // response.send('<h1>Hello, World!</h1>');
-   console.log(__dirname);
-   response.sendFile(__dirname+"/index.html");
+   //console.log(__dirname);
+   //response.sendFile(__dirname+"/index.html");
 })
 
-app.post('/', function(req,res){
+/*app.post('/', function(req,res){
     let number1= Number(req.body.num1);
     let number2= Number(req.body.num2);
 
@@ -19,7 +50,7 @@ app.post('/', function(req,res){
     res.send(`${result}`);
     // console.log('Number 1 ', number1);
     // console.log('Number 2 ', number2);
-});
+});*/
 app.listen(3000, function(){
     console.log('Server is running on Port 3000')
 })
